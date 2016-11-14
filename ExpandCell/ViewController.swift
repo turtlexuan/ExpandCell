29c197c2ece022629ee7bab3b955b193bf6b0214
turtlexuan <turtlexuan@gmail.com>
2016-11-14 20:21:49 +0800

    no message

--- ExpandCell/ViewController.swift
+++ ExpandCell/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    struct Section {
+        var name: String!
+        var items: [String]!
+        var collapsed: Bool!
+        
+        init(name: String, items: [String], collapsed: Bool = false) {
+            self.name = name
+            self.items = items
+            self.collapsed = collapsed
+        }
+    }
+    
+    var sections = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sections =
+        [
+            Section(name: "Mac", items: ["MacBook", "MacBook Air", "MacBook Pro", "iMac", "Mac Pro", "Mac mini", "Accessories", "OS X El Capitan"]),
+            Section(name: "iPad", items: ["iPad Pro", "iPad Air 2", "iPad mini 4", "Accessories"]),
+            Section(name: "iPhone", items: ["iPhone 6s", "iPhone 6", "iPhone SE", "Accessories"])
+        ]
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //
+        return sections[section].collapsed! ? 0 : sections[section].items.count
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        //
+        let header = tableView.dequeueReusableCellWithIdentifier("header") as! TableViewHeader!
+        
+        header.titleLabel.text = sections[section].name
+        header.toggleButton.tag = section
+        header.toggleButton.addTarget(self, action: #selector(ViewController.toggleCollapse(_:)), forControlEvents: .TouchUpInside)
+        header.toggleButton.rotate(sections[section].collapsed! ? 0.0 : CGFloat(M_PI_2))
+        
+        return header.contentView
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        
+        cell.textLabel?.text = sections[indexPath.section].items[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - Event Handlers
+    func toggleCollapse(sender: UIButton) {
+        let section = sender.tag
+        let collapsed = sections[section].collapsed
+        
+        // Toggle collapse
+        sections[section].collapsed = !collapsed
+        
+        // Reload section
+        tableView.reloadSections(NSIndexSet(index: section), withRowAnimation: .Automatic)
+    }
 
+    
 }
+
+extension UIView {
+    func rotate(toValue: CGFloat, duration: CFTimeInterval = 0.2) {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        
+        rotateAnimation.toValue = toValue
+        rotateAnimation.duration = duration
+        rotateAnimation.removedOnCompletion = false
+        rotateAnimation.fillMode = kCAFillModeForwards
+        
+        self.layer.addAnimation(rotateAnimation, forKey: nil)
+    }
+}
+
 
